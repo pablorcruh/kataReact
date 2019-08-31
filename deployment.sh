c0e76c1d0672ce73229bdca6df7ad3b3d232aeea
pablorcruh <pablorcruh@gmail.com>
2019-08-31 14:12:07 -0500

    cambios para despliegue dentro del cluster

--- deployment.sh
+++ deployment.sh
@@ -10,16 +10,16 @@ gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-ser
 
 gcloud config set project coding-dojo-251421
 
-gcloud --quiet config set container/cluster coding-dojo
+gcloud --quiet config set container/cluster coding-dojo-pablo-cruz
 
-gcloud config set compute/zone us-central1-b
+gcloud config set compute/zone us-central1-c
 
-gcloud --quiet container clusters get-credentials coding-dojo
+gcloud --quiet container clusters get-credentials coding-dojo-pablo-cruz
 
 docker build -t gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1 .
 
 gcloud docker -- push gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
 
-kubectl set image deployment/coding-dojo coding-dojo=gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
+kubectl set image deployment/coding-dojo-pablo-cruz coding-dojo=gcr.io/coding-dojo-251421/coding-dojo:$CIRCLE_SHA1
 
 echo " Successfully deployed"
